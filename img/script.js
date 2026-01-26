e478ecb16ce9165010fca67acde926023970d45f
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-01-26 15:57:04 +0100

    milestone 2 finito

--- img/script.js
+++ img/script.js
@@ -1,3 +1,46 @@
 // ho eseguito GET su endpoint API tramite Postman
-// mi ha tornato un array con 6 oggetti
+// mi ha tornato un array con 6 oggetti/
+//**
+// 
+/* [
+    {
+        "id": 1,
+        "title": "Skate Park",
+        "date": "01-07-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/1.png"
+    },
+    {
+        "id": 2,
+        "title": "Passeggiata",
+        "date": "16-07-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/2.png"
+    },
+    {
+        "id": 3,
+        "title": "Alpi",
+        "date": "01-07-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/3.png"
+    },
+    {
+        "id": 4,
+        "title": "Sagra",
+        "date": "21-08-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/4.png"
+    },
+    {
+        "id": 5,
+        "title": "Watergun",
+        "date": "23-08-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/5.png"
+    },
+    {
+        "id": 6,
+        "title": "Riviera",
+        "date": "30-08-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/6.png"
+    }
+] */
+//ogniuno contiene : id, tittle, date, url dell'immagine
+
+//questi dati mi serviranno per genereare la card dinamica che serve per il milestone 3
 
